5e9bd3324a4b293d5dbfd29fa1d3929e826f18fa
xoakzxz <juandavid_ja@hotmail.com>
2017-05-26 11:12:44 -0500

    Team service

--- routes/team.js
+++ routes/team.js
@@ -7,17 +7,22 @@ var database = require('./../database.js');
 //### Get Team [GET]
 router.get('/:playerId', function(request, response) {
   var playerId = request.params.playerId;
-  var query = '';
+  var query = 'SELECT "character_id_1", "character_id_2", "character_id_3" FROM "team" WHERE "player_id" IN ({0});';
+  query = query.replace('{0}', playerId);
 
   try {
     database.query(query, function(result){
       var responseJson = {
-
+        "character1": {"id": result[0].character_id_1},
+        "character2": {"id": result[0].character_id_2},
+        "character3": {"id": result[0].character_id_3},
       }
       response.send(responseJson);
-    })
+    });
 
   } catch (error) {
     console.error(error);
   }
 });
+
+module.exports = router;
